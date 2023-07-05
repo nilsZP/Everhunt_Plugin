36828f7b75a97f62f941df4a9cc86c3ae2c4ab80
Cédric <work@cedricverlinden.com>
2023-07-05 18:54:04 +0200

    Temporary solution found for custom itemstack as recipe ingredient

--- src/main/java/me/nils/vdvcraftrevamp/items/Recipes.java
+++ src/main/java/me/nils/vdvcraftrevamp/items/Recipes.java
@@ -8,42 +8,52 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class Recipes {
-    public static void DaggerRecipe() {
-        ItemStack item = new Weapons().Dagger();
 
-        NamespacedKey key = new NamespacedKey(VDVCraftRevamp.getInstance(),"DaggerRecipe");
+	/*
+        This solution is not ideal, it uses this itemstack to reference
+        the exact same instance of the dagger itemstack in both recipes.
 
-        ShapedRecipe recipe = new ShapedRecipe(key, item);
+        Whenever reloading/restarting the server, the dagger
+        in the #DaggerURecipe will not work anymore.
+    */
+	static ItemStack dagger;
 
-        recipe.shape(" S ", " I ", "   ");
+	public static void DaggerRecipe() {
+		dagger = new Weapons().Dagger();
 
-        recipe.setIngredient('S', new ItemStack(Material.STICK));
-        recipe.setIngredient('I', new ItemStack(Material.IRON_INGOT));
+		NamespacedKey key = new NamespacedKey(VDVCraftRevamp.getInstance(), "DaggerRecipe");
 
-        Bukkit.addRecipe(recipe);
-    }
+		ShapedRecipe recipe = new ShapedRecipe(key, dagger);
 
-    public static void DaggerURecipe() {
-        ItemStack item = new Weapons().DaggerOfShatteredDimensions();
+		recipe.shape(" S ", " I ", "   ");
 
-        NamespacedKey key = new NamespacedKey(VDVCraftRevamp.getInstance(),"DaggerURecipe");
+		recipe.setIngredient('S', new ItemStack(Material.STICK));
+		recipe.setIngredient('I', new ItemStack(Material.IRON_INGOT));
 
-        ShapedRecipe recipe = new ShapedRecipe(key, item);
+		Bukkit.addRecipe(recipe);
+	}
 
-        recipe.shape("S S", "SRS", "SDS");
+	public static void DaggerURecipe() {
+		ItemStack item = new Weapons().DaggerOfShatteredDimensions();
 
-        recipe.setIngredient('S', new Materials().ShatteredShard());
-        recipe.setIngredient('R', new Materials().RippedDimension());
-        recipe.setIngredient('D', new Weapons().Dagger());
-        // TODO fix daggerpart of recipe not working
+		NamespacedKey key = new NamespacedKey(VDVCraftRevamp.getInstance(), "DaggerURecipe");
 
-        Bukkit.addRecipe(recipe);
-    }
-    public static void loadRecipes() {
-        DaggerRecipe();
-        DaggerURecipe();
-    }
+		ShapedRecipe recipe = new ShapedRecipe(key, item);
+
+		recipe.shape("S S", "SRS", "SDS");
+
+		recipe.setIngredient('S', new Materials().ShatteredShard());
+		recipe.setIngredient('R', new Materials().RippedDimension());
+		recipe.setIngredient('D', dagger);
+		// TODO fix daggerpart of recipe not working
+
+		Bukkit.addRecipe(recipe);
+	}
+
+	public static void loadRecipes() {
+		DaggerRecipe();
+		DaggerURecipe();
+	}
 }
