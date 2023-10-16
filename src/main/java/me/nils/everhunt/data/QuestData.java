4af68233f4561f577016457c7103963742c5b746
nilsZP <nils.vandevelde@bazandpoort.be>
2023-10-16 21:13:49 +0200

    update questdata

--- src/main/java/me/nils/everhunt/data/QuestData.java
+++ src/main/java/me/nils/everhunt/data/QuestData.java
@@ -1,46 +1,47 @@
 package me.nils.everhunt.data;
 
+import me.nils.everhunt.Everhunt;
+import me.nils.everhunt.constants.Ability;
+import me.nils.everhunt.constants.MobType;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 public class QuestData {
-    public static final HashMap<String, QuestData> questdata = new HashMap<>();
-    private final String uuid;
+    private final int playerID;
     private int number;
     private double completion;
-    private final YamlConfiguration configuration;
 
-    public QuestData(String uuid, int number, double completion) {
+    public QuestData(int playerID, int number, double completion) {
         this.completion = completion;
         this.number = number;
-        this.uuid = uuid;
+        this.playerID = playerID;
 
-        FileManager fileManager = new FileManager("questdata", uuid);
-        configuration = fileManager.getFile();
-        configuration.set("uuid",uuid);
-        configuration.set("number", String.valueOf(number));
-        configuration.set("completion", String.valueOf(completion));
-
-        fileManager.save();
-        questdata.put(uuid, this);
+        try {
+            Everhunt.getDatabase().run("INSERT INTO tblquest (playerID, questnumber, type) VALUES (" + playerID + "," + number + "," +
+                    completion).executeQuery();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void registerQuestData() {
-        List<File> files = FileManager.getFiles("questdata");
-        if (files == null) {
-            return;
-        }
-        for (File file : files) {
-            YamlConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
+        try {
+            ResultSet resultSet = Everhunt.getDatabase().run("SELECT * FROM tblquest").executeQuery();
 
-            String uuid = fileConfiguration.getString("uuid");
-            int number = fileConfiguration.getInt("number");
-            double completion = fileConfiguration.getDouble("completion");
+            while (resultSet.next()) {
+                int playerID = resultSet.getInt("playerID");
+                int number = resultSet.getInt("questnumber");
+                double completion = resultSet.getDouble("progress");
 
-            new QuestData(uuid,number,completion);
+                new QuestData(playerID,number,completion);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
@@ -48,16 +49,8 @@ public class QuestData {
         return number;
     }
 
-    public void setNumber(String uuid, int value) {
-        File file = FileManager.getFile("questdata",uuid);
-        YamlConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
-        fileConfiguration.set("number", String.valueOf(value));
-        try {
-            fileConfiguration.save(file);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        this.number = value;
+    public void setNumber(int playerID, int value) {
+        // TODO fix this
     }
 
     public double getCompletion() {
@@ -65,14 +58,6 @@ public class QuestData {
     }
 
     public void setCompletion(String uuid, double value) {
-        File file = FileManager.getFile("questdata",uuid);
-        YamlConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
-        fileConfiguration.set("completion", String.valueOf(value));
-        try {
-            fileConfiguration.save(file);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        this.completion = value;
+        // TODO also this
     }
 }
