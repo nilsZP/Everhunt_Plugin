bba4a6e72e25676197324afd7a18840229959dc9
nilsZP <nils.vandevelde@bazandpoort.be>
2023-11-16 19:17:55 +0100

    add loottables

--- src/main/java/me/nils/everhunt/listeners/EntityListener.java
+++ src/main/java/me/nils/everhunt/listeners/EntityListener.java
@@ -1,8 +1,11 @@
 package me.nils.everhunt.listeners;
 
+import me.nils.everhunt.constants.MobType;
 import me.nils.everhunt.data.EntityData;
 import me.nils.everhunt.data.LootData;
 import me.nils.everhunt.entities.Hologram;
+import me.nils.everhunt.entities.loottables.Monster_Loot;
+import me.nils.everhunt.entities.loottables.WolfKing_Loot;
 import me.nils.everhunt.items.armor.MechanicalChestplate;
 import me.nils.everhunt.items.armor.SpringerBoots;
 import me.nils.everhunt.items.armor.UnitedHelmet;
@@ -11,10 +14,10 @@ import me.nils.everhunt.managers.ItemManager;
 import me.nils.everhunt.utils.Chat;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
+import org.bukkit.Location;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -22,16 +25,19 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.loot.LootContext;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Random;
 
 public class EntityListener implements Listener {
     @EventHandler
     public void onDeath(EntityDeathEvent event) {
+        event.getDrops().clear();
         if (!(event.getEntity() instanceof Player)) {
-            Entity entity = event.getEntity();
+            LivingEntity entity = event.getEntity();
             List<Entity> entityList = entity.getPassengers();
             for (Entity passenger : entityList) {
                 if (passenger instanceof ArmorStand hologram) {
@@ -42,21 +48,53 @@ public class EntityListener implements Listener {
             if (data == null) {
                 return;
             }
-            int id = data.getEntityID();
-            Random random = new Random();
-            ArrayList<Integer> dropList = LootData.getItemIDs(id);
-            for (int drop : dropList) {
-                double randomDouble = random.nextDouble(0,101);
-                if (randomDouble <= LootData.getChance(id,drop)) {
-                    int randomInteger = random.nextInt(LootData.getMinimum(id,drop),LootData.getMaximum(id,drop)+1);
-                    for (int i = 0; i < randomInteger; i++) {
-                        ItemStack item = ItemManager.items.get(ItemManager.getDisplayName(drop)).getItemStack();
-                        if (item != null) {
-                            entity.getWorld().dropItemNaturally(entity.getLocation(), item);
-                        }
+
+            int looting_mod;
+            double luck_mod;
+
+            Player player = entity.getKiller();
+
+            if (entity.getKiller() == null) {
+                looting_mod = 0;
+                luck_mod = 0;
+            } else {
+                looting_mod = player.getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.LOOT_BONUS_MOBS);
+                luck_mod = player.getAttribute(Attribute.GENERIC_LUCK).getValue();
+            }
+
+            Location location = entity.getLocation();
+
+            LootContext.Builder builder = new LootContext.Builder(location);
+            builder.lootedEntity(event.getEntity());
+            builder.lootingModifier(looting_mod);
+            builder.luck((float) luck_mod);
+            builder.killer(player);
+            LootContext lootContext = builder.build();
+
+            if (data.getDisplayName().equals("Wolf King")) {
+                WolfKing_Loot wolfKing_loot = new WolfKing_Loot();
+                Collection<ItemStack> drops = wolfKing_loot.populateLoot(new Random(), lootContext);
+                ArrayList<ItemStack> items = (ArrayList<ItemStack>) drops;
+
+                for (ItemStack item : items) {
+                    if (item.getAmount() > 0) {
+                        location.getWorld().dropItemNaturally(location, item);
                     }
                 }
             }
+
+            if (data.getType().equals(MobType.ENEMY) || data.getType().equals(MobType.BOSS)) {
+                Monster_Loot monster_loot = new Monster_Loot();
+                Collection<ItemStack> drops = monster_loot.populateLoot(new Random(), lootContext);
+                ArrayList<ItemStack> items = (ArrayList<ItemStack>) drops;
+
+                for (ItemStack item : items) {
+                    if (item.getAmount() > 0) {
+                        location.getWorld().dropItemNaturally(location, item);
+                    }
+                }
+            }
+
         }
     }
 
