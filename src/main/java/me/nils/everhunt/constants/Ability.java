f7bbbc7ef583219dc5afdabf55508e55e90e6eed
nilsZP <nils.vandevelde@bazandpoort.be>
2023-12-05 15:04:26 +0100

    add ability description

--- src/main/java/me/nils/everhunt/constants/Ability.java
+++ src/main/java/me/nils/everhunt/constants/Ability.java
@@ -1,5 +1,9 @@
 package me.nils.everhunt.constants;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public enum Ability {
     METEOR_BLAST("Meteor Blast", 1.3, 1,Activation.RIGHT_CLICK,10),
     EVOCATION("Evocation", 2, 5,Activation.RIGHT_CLICK,6),
@@ -7,7 +11,7 @@ public enum Ability {
     LETHAL_ABSORPTION("Lethal Absorption", 0, 20,Activation.RIGHT_CLICK,2),
     THUNDER_WARP("Thunder Warp", 1.1, 2,Activation.THROW,5),
     DIMENSION_UNISON("Dimension Unison", 0, 10,Activation.RIGHT_CLICK,50),
-    SNOWBALL("Snowball",0.5,0,Activation.RIGHT_CLICK,1),
+    SNOWBALL("Snowball",0.5,0,Activation.RIGHT_CLICK,1,"&7shoots a snowball dealing &c50% &7damage."),
     CLAP("Clap",0.8,4,Activation.LEFT_CLICK,0),
     THUNDER_CLAP("Thunder Clap",1.2,4,Activation.LEFT_CLICK,0),
     THUNDER_FLASH("Thunder Flash",1.5,4,Activation.LEFT_CLICK,0),
@@ -26,13 +30,15 @@ public enum Ability {
     private final int cooldown;
     private final Activation activation;
     private final int flowCost;
+    private final List<String> description;
 
-    Ability(String name, double damageMultiplier, int cooldown, Activation activation, int flowCost) {
+    Ability(String name, double damageMultiplier, int cooldown, Activation activation, int flowCost, String... description) {
         this.name = name;
         this.damageMultiplier = damageMultiplier;
         this.cooldown = cooldown;
         this.activation = activation;
         this.flowCost = flowCost;
+        this.description = Arrays.asList(description);
     }
 
     public String getName() {
@@ -53,5 +59,9 @@ public enum Ability {
 
     public int getFlowCost() {
         return flowCost;
+    }
+
+    public List<String> getDescription() {
+        return description;
     }
 }
