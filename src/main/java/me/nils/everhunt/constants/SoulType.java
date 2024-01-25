a2f1351d5ad3941403465ce7f1d711dc2de5bd6b
nilsZP <nils.vandevelde@bazandpoort.be>
2024-01-25 13:29:11 +0100

    add conquerer soultype

--- src/main/java/me/nils/everhunt/constants/SoulType.java
+++ src/main/java/me/nils/everhunt/constants/SoulType.java
@@ -5,8 +5,9 @@ import org.bukkit.ChatColor;
 public enum SoulType {
     BASIC(ChatColor.WHITE),
     KING(ChatColor.GOLD),
-    BOSS(ChatColor.DARK_RED),
-    ALPHA(ChatColor.DARK_AQUA);
+    BOSS(ChatColor.LIGHT_PURPLE),
+    ALPHA(ChatColor.DARK_AQUA),
+    CONQUERER(ChatColor.DARK_RED);
 
     private final ChatColor color;
 
