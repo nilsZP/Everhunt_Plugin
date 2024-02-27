acc59e2bae80766108326649639f16885d11a879
nilsZP <nils.vandevelde@bazandpoort.be>
2024-02-27 14:56:16 +0100

    Update BrokenBlock.java

--- src/main/java/me/nils/everhunt/utils/BrokenBlock.java
+++ src/main/java/me/nils/everhunt/utils/BrokenBlock.java
@@ -1,12 +1,16 @@
 package me.nils.everhunt.utils;
 
+import me.nils.everhunt.Everhunt;
 import me.nils.everhunt.constants.Ability;
 import me.nils.everhunt.entities.loottables.Loot;
 import me.nils.everhunt.managers.ToolManager;
 import org.bukkit.ChatColor;
 import org.bukkit.Effect;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitScheduler;
 
 import java.util.Date;
 
@@ -16,10 +20,12 @@ public class BrokenBlock {
     private int oldAnimation;
     private double damage = 0;
     private Block block;
+    private Material material;
 
     public BrokenBlock(Block block, int time){
         this.block = block;
         this.time = time;
+        this.material = block.getType();
     }
 
     public void incrementDamage(Player from, double multiplier){
@@ -57,6 +63,13 @@ public class BrokenBlock {
         breaker.breakBlock(block);
         BrokenBlocksService.removeBrokenBLock(block.getLocation());
         block.getLocation().getWorld().dropItemNaturally(block.getLocation(), Loot.getlootTable(block,ability).getRandom());
+        block.setType(Material.BEDROCK);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                block.setType(material);
+            }
+        }.runTaskLater(Everhunt.getInstance(),100);
     }
 
     public void destroyBlockObject(Player player){
